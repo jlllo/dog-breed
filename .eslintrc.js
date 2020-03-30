81756b98599162f18442a14a56e7a0dc3edc998e
jlllo <jeziellcarvalho@gmail.com>
2020-03-30 17:11:46 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "import", "jsx-a11y"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"]
+      }
+    ],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ],
+    "linebreak-style": 0,
+    "no-multiple-empty-lines": [1, { max: 1, maxEOF: 1 }],
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: "always",
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: "never",
+        ExportDeclaration: { multiline: true, minProperties: 3 }
+      }
+    ],
+    "react/jsx-props-no-spreading": "off",
+    "no-explicit-any": 0,
+    "max-len": 0
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {}
+    }
+  }
+};
+
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es6: true
+//   },
+//   extends: ["plugin:react/recommended", "airbnb"],
+//   globals: {
+//     Atomics: "readonly",
+//     SharedArrayBuffer: "readonly"
+//   },
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//       modules: true
+//     },
+//     ecmaVersion: 2018,
+//     sourceType: "module"
+//   },
+//   plugins: ["react", "@typescript-eslint"],
+//   rules: {}
+// };
