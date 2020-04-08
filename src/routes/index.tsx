c02f21067e85d5787fed27c5690a01aabe7a95ad
jlllo <jeziellcarvalho@gmail.com>
2020-04-08 18:08:12 -0300

    update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import TopTabDogs from '../containers/TopTabDogs';
+import { Button, Icon } from 'react-native-elements';
+import { useDispatch } from 'react-redux';
+import Dogs from '../pages/Dogs';
 import Register from '../pages/Register';
+import { clearDogs, setLoading, setToken } from '../store/ducks/DogsBreedData/actions';
 import { theme } from '../styles/global';
 
 const Stack = createStackNavigator();
 
 function Routes() {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(setToken(''));
+    dispatch(clearDogs());
+    dispatch(setLoading(false));
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Register">
@@ -34,9 +45,27 @@ function Routes() {
           }}
         />
         <Stack.Screen
-          name="ListDogs"
-          component={TopTabDogs}
+          name="Dogs"
+          component={Dogs}
           options={{
+            headerRight: () => (
+              <Button
+                onPress={handleLogout}
+                icon={(
+                  <Icon
+                    name="power-off"
+                    type="font-awesome"
+                    size={20}
+                    color="white"
+                  />
+                )}
+                buttonStyle={{
+                  backgroundColor: theme.primaryColor,
+                  padding: 15,
+                  borderRadius: 50,
+                }}
+              />
+            ),
             title: 'Dogs',
             headerTitleStyle: {
               flexGrow: 1,
